7e413036b229927effe85078ab1003a1c4af00b7
HouyuwenLu <64843384+HouyuwenLu@users.noreply.github.com>
2021-02-15 13:33:49 -0500

    Add files via upload

--- NimPlayer.java
+++ NimPlayer.java
@@ -1,4 +1,16 @@
-
+/**
+ * The university of Melbourne
+ * School of Computing and Information Systems 
+ * COMP90041 Programming and Software Development 
+ * Lecturer: Dr Tilman Dingler, Dr Thuan Pham 
+ * Semester 2, September 14th, 2020
+ * Project 1, The Game of Nim 
+ * Student Name: Houyuwen Lu 
+ * Student Number: 1205947
+ * Github repository link: https://github.com/COMP90041/assignment-1-HouyuwenLu-COMP90041.git
+ * NimPlayer class and invoked in Nimsys class
+ * @author Houyuwen Lu
+ */
 public class NimPlayer {
 	// declare the name, wins, and games as instance variables in the class NimPlayer
 	private String name;
